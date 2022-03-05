2c457e6af047fad6ce4b6ffe5deef7ddef3beeaa
seeyoulateraligatorinawhilepedophile <55834060+seeyoulateraligatorinawhilepedophile@users.noreply.github.com>
2022-03-05 14:40:45 +0100

    add test file

--- README.md
+++ README.md
@@ -3,8 +3,10 @@ Repository for the project of group cs-22-dv-4-03
 
 
 The files in test for yelp datasets won't work, cause the json files are too big.
+A short file for business is added in Test for yelp datasets, in case you don't have a file.
 
 
 ###todo
 
 data cleaning of yelp_academic_dataset_business.json, yelp_academic_dataset_review.json and yelp_academic_dataset_user.json.
+
--- Test for yelp datasets/graf_business.py
+++ Test for yelp datasets/graf_business.py
@@ -12,22 +12,24 @@ from rdflib import Graph, Namespace, URIRef, BNode, Literal
 from rdflib.namespace import RDF, FOAF, XSD
 
 
-#list to save each 
+#list to save each fragment
 data_business = []
 n = 0
 #open json file line for line
-with open('yelp_academic_dataset_business.json', encoding="utf8") as f:
+#smaller size for original file yelp_academic_dataset_business_10000lines.json
+#real file yelp_academic_dataset_business.json
+with open('yelp_academic_dataset_business_10000lines.json', encoding="utf8") as f:
     for line in f:
         data_business.append(json.loads(line))
 
-#
+#predicator
 schema = Namespace("https://schema.org/")
 g = Graph()
 
 
 #transform the list and append to Graph 
 
-for i in data_business[0:10]:
+for i in data_business:
     if len(data_business[n]['name']) > 0 and data_business[n]['name'] != None:
         g.add((Literal(data_business[n]['business_id']), schema.legalName, Literal(data_business[n]['name'])))
     if len(data_business[n]['address']) > 0:
