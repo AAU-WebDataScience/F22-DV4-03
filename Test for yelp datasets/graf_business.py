c652179fc8bb86aa0c1267e501f6c948cc839158
seeyoulateraligatorinawhilepedophile <55834060+seeyoulateraligatorinawhilepedophile@users.noreply.github.com>
2022-03-08 15:05:37 +0100

    Update graf_business.py

--- Test for yelp datasets/graf_business.py
+++ Test for yelp datasets/graf_business.py
@@ -24,38 +24,45 @@ with open('yelp_academic_dataset_business_10000lines.json', encoding="utf8") as 
 
 #predicator
 schema = Namespace("https://schema.org/")
+yelp_business = Namespace("https://www.yelp.com/biz/")
+
 g = Graph()
 
 
 #transform the list and append to Graph 
 
+
 for i in data_business:
     if len(data_business[n]['name']) > 0 and data_business[n]['name'] != None:
-        g.add((Literal(data_business[n]['business_id']), schema.legalName, Literal(data_business[n]['name'])))
+        g.add((URIRef(yelp_business + data_business[n]['business_id']), schema.legalName, Literal(data_business[n]['name'])))
     if len(data_business[n]['address']) > 0:
-        g.add((Literal(data_business[n]['business_id']), schema.streetAddress, Literal(data_business[n]['address'])))
+        g.add((URIRef(yelp_business + data_business[n]['business_id']), schema.streetAddress, Literal(data_business[n]['address'])))
     
     #is all the data from the us?
     if len(data_business[n]['state']) > 0:
-        g.add((Literal(data_business[n]['business_id']), schema.addressRegion, Literal(data_business[n]['state'])))
+        g.add((URIRef(yelp_business + data_business[n]['business_id']), schema.addressRegion, Literal(data_business[n]['state'])))
     if len(data_business[n]['postal_code']) > 0:
-        g.add((Literal(data_business[n]['business_id']), schema.postalCode, Literal(data_business[n]['postal_code'])))
+        g.add((URIRef(yelp_business + data_business[n]['business_id']), schema.postalCode, Literal(data_business[n]['postal_code'])))
     
     #the len does not apply for doubles or int
     if len(str(data_business[n]['latitude'])) > 0:
-        g.add((Literal(data_business[n]['business_id']), schema.latitude, Literal(data_business[n]['latitude'])))
+        g.add((URIRef(yelp_business + data_business[n]['business_id']), schema.latitude, Literal(data_business[n]['latitude'])))
     if len(str(data_business[n]['longitude'])) > 0:
-        g.add((Literal(data_business[n]['business_id']), schema.longitude, Literal(data_business[n]['longitude'])))
+        g.add((URIRef(yelp_business + data_business[n]['business_id']), schema.longitude, Literal(data_business[n]['longitude'])))
     if len(str(data_business[n]['stars'])) > 0:
-        g.add((Literal(data_business[n]['business_id']), schema.starRating, Literal(data_business[n]['stars'])))
+        g.add((URIRef(yelp_business + data_business[n]['business_id']), schema.starRating, Literal(data_business[n]['stars'])))
     n = n + 1
     #these 2 line have have problems with predicate 
     #g.add((Literal(data[n]['business_id']), schema.ratingCount, Literal(data[n]['ratingCount'])))
     #g.add((Literal(data[n]['business_id']), schema.brand, Literal(data[n]['categories'])))
     
     
+    
 #print 3 variabel for graph in g
 for s, p, o in g:
-    print(s+"     "+p+"     "+o)
+    print(s+'  '+p+'  '+o)
     
+    #http://dbpedia.org/resource/United_States
+    #https://dbpedia.org/page/California 
+    #http://dbpedia.org/resource/California
     
