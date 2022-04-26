81aab0252c309cfa1cb93a9fe3091e004de5081c
skingi20 <skingi20@student.aau.dk>
2022-04-26 13:44:53 +0200

    counted states, cities

---
+++ misc/make_bar_graph_states.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+library(ggplot2)
+my_data <- read.csv("D:/Documents - HDD/GitHub/F22-DV4-03/misc/amount_of_states.csv")
+
+
+ggplot(my_data, aes(x = reorder(State, -Amount), y=Amount))+geom_bar(stat = "identity", color="#211A52", fill="#211A52")+xlab("State code")+theme_minimal()
