97f6071437875fc1e31cf3caf3450434ecb700f7
marcusaustin78 <55834060+marcusaustin78@users.noreply.github.com>
2022-05-10 10:35:45 +0200

    Update generate_tip_graph.py

--- Generate graphs py/generate_tip_graph.py
+++ Generate graphs py/generate_tip_graph.py
@@ -8,14 +8,7 @@ import json
 
 from rdflib import Graph, Namespace, URIRef, Literal, BNode
 from rdflib.namespace import XSD
-
-#list to save the data for each checkin
-data_tip = []
-
-#populating the list
-with open('yelp_academic_dataset_tip.json', encoding="utf8") as f:
-    for line in f:
-        data_tip.append(json.loads(line))
+import gc
 
 #schema.org prefix
 schema = Namespace("https://schema.org/")
@@ -26,35 +19,42 @@ yelp_business = Namespace("https://www.yelp.com/biz/")
 #custom URI - used for predicate 'number of compliments'
 yelp_tip = Namespace("https://www.yelp.com/kg/tip/")
 
-g = Graph()
+n = 0
 
-def tip_author_rdf(): #schema.author
-    n = 0
-    for i in data_tip[:10]:
+with open('yelp_academic_dataset_tip.json', encoding="utf8") as f:
+    for line in f:
+        line = json.loads(line)
         bn = BNode(n)
-        if data_tip[n]['user_id'] != None:
-            g.add((URIRef(yelp_user + data_tip[n]['user_id']),
+        g = Graph()
+        if line["user_id"] != None:
+            g.add((URIRef(yelp_user + line['user_id']),
                    schema.author, 
                    bn))
-        if data_tip[n]['business_id'] != None:
+        if line['business_id'] != None:
             g.add((bn, 
                    schema.about, 
-                   URIRef(yelp_business + data_tip[n]['business_id'])))
-        if data_tip[n]['date'] != None:
+                   URIRef(yelp_business + line['business_id'])))
+        if line['date'] != None:
             g.add((bn, 
                    schema.dateCreated, 
-                   Literal(data_tip[n]['date'], datatype=XSD.dateTime)))
-        if data_tip[n]['compliment_count'] != None: #This also generates objects where the count = 0
+                   Literal(line['date'], datatype=XSD.dateTime)))
+        if line['compliment_count'] != None: #This also generates objects where the count = 0
             g.add((bn, 
                    URIRef(yelp_tip + "compliment_count"), 
-                   Literal(data_tip[n]['compliment_count'], datatype=XSD.int)))
-        if data_tip[n]['text'] != None: #This also generates objects where the count = 0
+                   Literal(line['compliment_count'], datatype=XSD.int)))
+        if line['text'] != None: #This also generates objects where the count = 0
             g.add((bn, 
                    schema.text, 
-                   Literal(data_tip[n]['text'], datatype=XSD.string)))
-        n += 1
-
-tip_author_rdf()
-
-#change this:
-g.serialize(format="xml", destination="tip_graph.xml")
+                   Literal(line['text'], datatype=XSD.string)))
+        g1 = Graph()
+        g1.parse("file1.ttl",format="turtle")        
+        
+        g2 = g + g1
+            
+        g.serialize(format="ttl", destination="file1.ttl")
+        
+        gc.collect()
+        
+        n = n + 1
+        
+        print(n)
