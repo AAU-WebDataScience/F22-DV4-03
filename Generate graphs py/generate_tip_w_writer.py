1ee39727231a7399236a9933271f0b947ac12524
jrrp20 <73336048+jrrp20@users.noreply.github.com>
2022-05-11 19:15:57 +0200

    Update generate_tip_w_writer.py

--- Generate graphs py/generate_tip_w_writer.py
+++ Generate graphs py/generate_tip_w_writer.py
@@ -9,11 +9,10 @@ import json
 
 from rdflib import Graph, Namespace, URIRef, Literal, BNode
 from rdflib.namespace import XSD
-import gc
 
 #schema.org prefix
 schema = Namespace("https://schema.org/")
-#to map the users id:
+#to map the user's id:
 yelp_user = Namespace("https://www.yelp.com/user_details?userid=")
 #to map the biz
 yelp_business = Namespace("https://www.yelp.com/biz/")
@@ -27,35 +26,34 @@ n = 0
 with open('yelp_academic_dataset_tip.json', encoding="utf8") as f:
     for line in f:
         line = line.encode("ascii", "ignore") #removing unicode characters
-        line = json.loads(line)
-        bn = BNode(n)
-        g = Graph()
-        
-        if line["user_id"] != None:
+        line = json.loads(line) #reading in the line from the .json file
+        bn = BNode(n) #creating a blank node
+        g = Graph() #creating a graph
+        if line["user_id"] != None: #a user is the author of the tip
             g.add((URIRef(yelp_user + line['user_id']),
                    schema.author, 
                    bn))
-        if line['business_id'] != None:
+        if line['business_id'] != None: #the tip is about some biz
             g.add((bn, 
                    schema.about, 
                    URIRef(yelp_business + line['business_id'])))
-        if line['date'] != None:
+        if line['date'] != None: #the tip was created at a date
             g.add((bn, 
                    schema.dateCreated, 
                    Literal(line['date'], datatype=XSD.dateTime)))
-        if line['compliment_count'] != None: #This also generates objects where the count = 0
+        if line['compliment_count'] != None: #the tip has received # compliments
             g.add((bn, 
                    URIRef(yelp_tip + "compliment_count"), 
                    Literal(line['compliment_count'], datatype=XSD.int)))
-        if line['text'] != None: #This also generates objects where the count = 0
+        if line['text'] != None: #the tip has a text
             g.add((bn, 
                    schema.text, 
                    Literal(line['text'], datatype=XSD.string)))
         
-        graph_file.write(g.serialize(format="nt"))
+        graph_file.write(g.serialize(format="nt")) #appending the triples to the graph file
         
-        n = n + 1
+        print(n) #counting the number of lines transformed to rdf (-1)
         
-        print(n)
+        n += 1
         
 graph_file.close()
